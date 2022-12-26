82b6c82015e2ddc1053a2090e876e1288b6e3b28
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2022-12-26 19:46:06 +0900

    Create 11654.cpp

---
+++ basic io/11654.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char c;
+int val;
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> c; //문자열 받기
+    val = c; //아스키코드값을 정수형 val 변수에 
+    
+    cout << val;
+}
