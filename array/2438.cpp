11bd42a452e6118c36ec57bdefecc4fca6629e92
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-01-03 22:37:45 +0900

    Create 2438.cpp

---
+++ array/2438.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+
+		cout << '\n';
+	}
+}
