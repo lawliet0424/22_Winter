20b50cf9337ca07c03824a7fc3f1c7ec0fe0ddc6
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-01-08 15:45:26 +0900

    Create 11931.cpp

---
+++ ICPC/1회차/11931.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int arr[1000001];
+
+bool compare(int& a, int& b) {
+	return a > b;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n, compare);
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << '\n';
+
+	return 0;
+}
