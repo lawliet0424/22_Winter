16f7c780b8ade0be4bf7ceec6ee3119bd587df8e
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-01-08 15:46:47 +0900

    Create 2751.cpp

---
+++ ICPC/1회차/2751.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int arr[1000001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + n);
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << '\n';
+
+	return 0;
+}
