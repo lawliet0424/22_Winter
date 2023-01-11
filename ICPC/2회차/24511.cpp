6c8c20ce5b6b75faee229c864e27fffcfec4903f
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2023-01-11 14:14:37 +0900

    Create 24511.cpp

---
+++ ICPC/2회차/24511.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <deque>
+using namespace std;
+
+deque <int> arr;
+int order[100001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, m, val;
+	int sum = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> order[i];
+
+	for (int i = 0; i < n; i++) {
+		cin >> val;
+		if (order[i] == 0)
+			arr.push_back(val);
+	}
+
+	cin >> m;
+	while (m--) {
+		cin >> val;
+
+		arr.push_front(val);
+		cout << arr.back() << " "; //arr.back()은 deque 'arr'의 마지막 원소를 반환
+		arr.pop_back();
+	}
+	return 0;
+}
